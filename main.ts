e75cc0d4aca64335f9937bd38efb2fd0927ca382
XavierFarrus <xavier.farrus@gracia.lasalle.cat>
2025-12-08 19:22:47 +0100

    Apartat b del pdf

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
 basic.forever(function () {
-	
+    basic.showNumber(input.temperature())
+    if (input.temperature() > 22) {
+        basic.showString("Calor")
+    } else {
+        basic.showString("Fred")
+    }
+    if (input.lightLevel() > 200) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.BaDing), music.PlaybackMode.InBackground)
+    }
+    if (input.lightLevel() < 50) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Blues), music.PlaybackMode.InBackground)
+    } else {
+    	
+    }
 })
